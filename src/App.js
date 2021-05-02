a92d5c48d3f7c5105d11df4d91af7a98730e9cbb
YonJun <jhon_08_2007@hotmail.com>
2021-05-02 17:16:58 -0500

    grid

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/** @jsxImportSource @emotion/react */
+import tw, { styled } from "twin.macro";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
@@ -5,22 +7,33 @@ import FirstItem from "./pages/FirstItem";
 import Sidebar from "./layouts/Sidebar";
 import Cover from "./components/Cover";
 
+const NavContainer = styled.div`
+  background: rgb(29, 30, 44, 0.8);
+  ${tw`h-full col-span-1`}
+`;
+
 export default function App() {
   return (
     <Router>
       <Cover>
-        <Sidebar />
-        <Switch>
-          <Route path="/item1">
-            <FirstItem />
-          </Route>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="*" exact>
-            <h1>404</h1>
-          </Route>
-        </Switch>
+        <div tw="h-full grid grid-cols-6">
+          <NavContainer>
+            <Sidebar />
+          </NavContainer>
+          <div tw="h-full col-span-5">
+            <Switch>
+              <Route path="/item1">
+                <FirstItem />
+              </Route>
+              <Route path="/" exact>
+                <Home />
+              </Route>
+              <Route path="*" exact>
+                <h1>404</h1>
+              </Route>
+            </Switch>
+          </div>
+        </div>
       </Cover>
     </Router>
   );
