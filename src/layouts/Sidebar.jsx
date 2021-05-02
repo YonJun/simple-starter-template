a1b4dc02602922ac200b60c5dbff515f34d066b7
YonJun <jhon_08_2007@hotmail.com>
2021-05-02 18:22:43 -0500

    update sidebar

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,13 +1,16 @@
 /** @jsxImportSource @emotion/react */
 import tw, { styled } from "twin.macro";
 import { Fragment } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 const ITEM_HEIGHT = 65;
 
+const Container = styled.div`
+  line-height: ${ITEM_HEIGHT}px;
+`;
+
 const Li = styled.li`
   height: ${ITEM_HEIGHT}px;
-  line-height: ${ITEM_HEIGHT}px;
 
   .selected::after {
     content: "";
@@ -28,23 +31,43 @@ const Li = styled.li`
 const Sidebar = () => {
   return (
     <Fragment>
-      <nav tw="grid grid-cols-6">
-        <ul tw="col-span-5 text-center  text-white uppercase font-semibold">
-          <li tw="my-14 font-black text-2xl">
-            <span tw="text-primary">your</span>logo
-          </li>
-          <Li tw="block relative">
-            <NavLink exact activeClassName="selected" to="/">
-              Home
-            </NavLink>
-          </Li>
-          <Li tw="block relative">
-            <NavLink exact activeClassName="selected" to="/about">
-              About
-            </NavLink>
-          </Li>
-        </ul>
-      </nav>
+      <Container tw="flex flex-col justify-between h-full lg:w-10/12  text-center">
+        <nav>
+          <ul tw="text-white uppercase font-semibold">
+            <li tw="my-14 font-black text-2xl">
+              <span tw="text-primary">your</span>logo
+            </li>
+            <Li tw="block relative">
+              <NavLink exact activeClassName="selected" to="/">
+                Home
+              </NavLink>
+            </Li>
+            <Li tw="block relative">
+              <NavLink exact activeClassName="selected" to="/item1">
+                Item 1
+              </NavLink>
+            </Li>
+            {[...new Array(4).keys()].map((i) => (
+              <Li tw="block relative" key={i}>
+                <Link exact to={`/${i}`}>
+                  Item {i + 2}
+                </Link>
+              </Li>
+            ))}
+          </ul>
+        </nav>
+        <nav tw="mb-10 uppercase text-gray-300">
+          <ul>
+            <li>support</li>
+          </ul>
+          <ul tw="mb-5">
+            <li>contact</li>
+          </ul>
+          <ul tw="text-gray-500">
+            <li>©2020 ACME LLC.</li>
+          </ul>
+        </nav>
+      </Container>
     </Fragment>
   );
 };
